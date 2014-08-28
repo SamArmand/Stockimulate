218324b28961acb5f24a3b9443d6a73902a60e8f
Sam Assaf <sam.assaf@outlook.com>
2014-08-28 04:57:27 +0000

    git-tfs-id: [https://samarmand.visualstudio.com]$/Stockimulate;C5

---
+++ JMSX/JMSX/Simulator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JMSX
+{
+    public class Simulator
+    {
+        public static int CaseNumber;
+        public static int DayNumber;
+
+        public static int Index1_Price;
+        public static int Index2_Price;
+
+        public static string Index1_NewsItem;
+        public static string Index2_NewsItem;
+
+
+
+    }
+}
