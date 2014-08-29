232d770157f1fbc1672162a7b10bcb126c981e07
Sam Assaf <sam.assaf@outlook.com>
2014-08-29 15:28:53 +0000

    git-tfs-id: [https://samarmand.visualstudio.com]$/Stockimulate;C6

--- JMSX/JMSX/DAO.cs
+++ JMSX/JMSX/DAO.cs
@@ -85,7 +85,7 @@ namespace JMSX
             Reader.Read();                               
                                     
             int BuyerPosition = Reader.GetInt32(Reader.GetOrdinal(Index)) + Quantity;
-            float BuyerFunds = Reader.GetFloat(Reader.GetOrdinal("Funds")) - (float)(Price * Quantity);
+            int BuyerFunds = Reader.GetInt32(Reader.GetOrdinal("Funds")) - (Price * Quantity);
 
             Reader.Dispose();
             Command.Dispose();
@@ -114,7 +114,7 @@ namespace JMSX
             Reader.Read();
 
             int SellerPosition = Reader.GetInt32(Reader.GetOrdinal(Index)) - Quantity;
-            float SellerFunds = Reader.GetFloat(Reader.GetOrdinal("Funds")) + (float)(Price * Quantity);
+            int SellerFunds = Reader.GetInt32(Reader.GetOrdinal("Funds")) + (Price * Quantity);
 
             Reader.Dispose();
             Command.Dispose();
@@ -124,11 +124,11 @@ namespace JMSX
 
         }
 
-        private void UpdatePlayerPositionBalance(int ID, string Index, int Position, float Funds)
+        private void UpdatePlayerPositionBalance(int ID, string Index, int Position, int Funds)
         {
             SqlConnection Connection = new SqlConnection(ConnectionString);
 
-            string Query = "UPDATE Players SET " + Index + "=@BuyerPosition, Funds=@Funds WHERE ID=@ID;";
+            string Query = "UPDATE Players SET " + Index + "=@Position, Funds=@Funds WHERE ID=@ID;";
                         
             SqlCommand Command = new SqlCommand(Query);
             Command.CommandType = CommandType.Text;
@@ -178,6 +178,57 @@ namespace JMSX
         public void ClosePositions()
         {
 
+            SqlConnection Connection = new SqlConnection(ConnectionString);
+            
+            string Query = "SELECT ID, PositionIndex1, PositionIndex2, Funds FROM Players WHERE NOT (ID=0)";
+
+            SqlCommand Command = new SqlCommand(Query);
+            Command.CommandType = CommandType.Text;
+
+            Connection.Open();
+
+            Command.Connection = Connection;
+
+            SqlDataReader Reader = Command.ExecuteReader();
+
+            List<Player> PlayersToUpdate = new List<Player>();
+
+            while (Reader.Read())
+            {
+                int ID = Reader.GetInt32(Reader.GetOrdinal("ID"));
+                int PositionIndex1 = Reader.GetInt32(Reader.GetOrdinal("PositionIndex1"));
+                int PositionIndex2 = Reader.GetInt32(Reader.GetOrdinal("PositionIndex2"));
+                int Funds = Reader.GetInt32(Reader.GetOrdinal("Funds"));
+
+                if (PositionIndex1 != 0 || PositionIndex2 != 0)
+                    PlayersToUpdate.Add(new Player(ID, PositionIndex1, PositionIndex2, Funds));
+
+            }
+
+            Reader.Dispose();
+            Command.Dispose();
+            Connection.Dispose();
+
+            foreach (Player Player in PlayersToUpdate) {
+
+                if (Player.GetPositionIndex1() > 0)
+                {
+                    InsertTrade(0, Player.GetID(), "SEC1", Player.GetPositionIndex1(), Simulator.Index1_Price);
+                }
+                else if (Player.GetPositionIndex1() < 0)
+                {
+                    InsertTrade(Player.GetID(), 0, "SEC1", (Player.GetPositionIndex1() * -1), Simulator.Index1_Price);
+                }
+                if (Player.GetPositionIndex2() > 0)
+                {
+                    InsertTrade(0, Player.GetID(), "SEC2", Player.GetPositionIndex1(), Simulator.Index1_Price);
+                }
+                else if (Player.GetPositionIndex1() < 0)
+                {
+                    InsertTrade(Player.GetID(), 0, "SEC2", (Player.GetPositionIndex1() * -1), Simulator.Index1_Price);
+                }
+
+            }
 
         }
 
---
+++ JMSX/JMSX/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JMSX
+{
+    public class Player
+    {
+        private int ID;
+        private int PositionIndex1;
+        private int PositionIndex2;
+        private int Funds;
+
+        public Player(int ID, int PositionIndex1, int PositionIndex2, int Funds) {
+            this.ID = ID;
+            this.PositionIndex1 = PositionIndex1;
+            this.PositionIndex2 = PositionIndex2;
+            this.Funds = Funds;
+        }
+
+        public int GetID()
+        {
+            return ID;
+        }
+
+        public int GetPositionIndex1()
+        {
+            return PositionIndex1;
+        }
+
+        public int GetPositionIndex2()
+        {
+            return PositionIndex2;
+        }
+
+        public int GetFunds()
+        {
+            return Funds;
+        }
+
+    }
+}
