487b82590eef6b2328d62332dfd806a6a1ded491
Sam Assaf <sam.assaf@outlook.com>
2015-11-08 20:42:21 +0000

    Modified override page
    git-tfs-id: [https://samarmand.visualstudio.com]$/Stockimulate;C81

--- JMSX/JMSX/Views/AdminViews/Override.aspx.designer.cs
+++ JMSX/JMSX/Views/AdminViews/Override.aspx.designer.cs
@@ -40,15 +40,6 @@ namespace Stockimulate.Views.AdminViews {
         protected global::System.Web.UI.WebControls.Button UpdatePrice;
         
         /// <summary>
-        /// ToggleReportsEnabled control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button ToggleReportsEnabled;
-        
-        /// <summary>
         /// ReportsEnabledSpan control.
         /// </summary>
         /// <remarks>
@@ -56,5 +47,14 @@ namespace Stockimulate.Views.AdminViews {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl ReportsEnabledSpan;
+        
+        /// <summary>
+        /// ToggleReportsEnabled control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ToggleReportsEnabled;
     }
 }
