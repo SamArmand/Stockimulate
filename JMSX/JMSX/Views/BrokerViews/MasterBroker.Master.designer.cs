ff7567b9474f2537aba7be9c57dff80b5e4f622c
Sam Assaf <sam.assaf@outlook.com>
2015-11-13 05:07:06 +0000

    Another out of bounds index fix
    git-tfs-id: [https://samarmand.visualstudio.com]$/Stockimulate;C86

--- JMSX/JMSX/Views/BrokerViews/MasterBroker.Master.designer.cs
+++ JMSX/JMSX/Views/BrokerViews/MasterBroker.Master.designer.cs
@@ -58,24 +58,6 @@ namespace Stockimulate.Views.BrokerViews {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl Index2PriceH2;
         
         /// <summary>
-        /// Index3SymbolH2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Index3SymbolH2;
-        
-        /// <summary>
-        /// Index3PriceH2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Index3PriceH2;
-        
-        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
