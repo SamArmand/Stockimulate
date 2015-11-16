d68676b7904a2b6241a591f038289fd026ad2c75
Sam Assaf <sam.assaf@outlook.com>
2015-11-16 04:10:16 +0000

    Unified Admin and Override Panel
    git-tfs-id: [https://samarmand.visualstudio.com]$/Stockimulate;C93

--- JMSX/JMSX/Views/AdminViews/Admin.aspx.cs
+++ JMSX/JMSX/Views/AdminViews/Admin.aspx.cs
@@ -1,14 +1,26 @@
 ﻿using System;
+using System.Web.UI.WebControls;
 
 namespace Stockimulate.Views.AdminViews
 {
     public partial class Admin : System.Web.UI.Page
     {
         private Simulator _simulator;
-        
+        private DataAccess _dataAccess;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             _simulator = Simulator.Instance;
+
+            _dataAccess = DataAccess.SessionInstance;
+
+            ReportsEnabledSpan.InnerHtml = "Reports Page Enabled: " + _dataAccess.IsReportsEnabled();
+
+            var instruments = _dataAccess.Instruments;
+
+            for (var i = 0; i < instruments.Count; ++i)
+                SecurityDropDownList.Items.Add(new ListItem(instruments[i].Symbol, i.ToString()));
+
         }
 
         protected void PlayPractice_Click(object sender, EventArgs e)
@@ -124,6 +136,27 @@ namespace Stockimulate.Views.AdminViews
             VerifyInput.Checked = false;
             //Clear other form fields
         }
+
+        protected void UpdatePrice_Click(object sender, EventArgs e)
+        {
+            if (PriceInput.Value != string.Empty && int.Parse(PriceInput.Value) >= 0)
+            {
+                var instrument = _dataAccess.GetInstruments()[SecurityDropDownList.SelectedIndex];
+                instrument.Price = int.Parse(PriceInput.Value);
+                _dataAccess.Update(instrument);
+            }
+
+            Response.Redirect("Override.aspx");
+
+        }
+
+        protected void ToggleReportsEnabled_Click(object sender, EventArgs e)
+        {
+            _dataAccess.UpdateReportsEnabled(_dataAccess.IsReportsEnabled() ? "False" : "True");
+
+            Response.Redirect("Override.aspx");
+        }
+
 
     }
 }
